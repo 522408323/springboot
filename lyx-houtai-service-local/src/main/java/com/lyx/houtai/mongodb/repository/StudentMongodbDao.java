4d12bf1a888360b5f5ae921f9fa898e30be1d797
shenyafei1 <shenyafei@labifenqi.com>
2020-08-31 18:48:01 +0800

    添加mongodb

---
+++ lyx-houtai-service-local/src/main/java/com/lyx/houtai/mongodb/repository/StudentMongodbDao.java
@@ -0,0 +1,14 @@
+package com.lyx.houtai.mongodb.repository;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName: StudentMongodbDao
+ * @Author: shenyafei
+ * @Date: 2020/8/31
+ * @Desc
+ **/
+@Component
+@Slf4j
+public class StudentMongodbDao {
+}
